39975831bdb5997fec99aa4e500da9937752aa19
Nikola Kurbalija <nikola.kurbalija@hotmail.com>
2021-04-04 19:45:57 +0200

    Unfinished genie script, fixed SSH parameters.

--- genieverif.py
+++ genieverif.py
@@ -1,15 +1,17 @@
 #! usr/bin/env python
 from genie.testbed import load
 from genie.utils.diff import Diff
+import json
 
 #Creating a testbed object for the network from yml file
 testbed = load("testbed.yml")
 
+
 device = testbed.devices["CoreRouter"]
 device.connect()
 
-current_output = device.parse("show protocols")
-diff = Diff(current_output, expected.output)
+current_interface_status = device.parse("show protocols")
+diff = Diff(current_interface_status, expected_interface_status)
 diff.findDiff()
 print("Difference between the expected output and actual output:\n","-"*50,diff)
 
